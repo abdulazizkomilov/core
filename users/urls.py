b8615489bc2bf4efc4b0c718d42cd5afb4756ff3
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-03-25 08:44:30 +0500

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import signup, vendor_detail
+
+app_name = 'users'
+
+urlpatterns = [
+    path('vendors/<int:pk>', vendor_detail, name='vendor_detail'),
+    path('signup', signup, name='signup'),
+]
+
+
