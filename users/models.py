3fa2efc04a9cf1b7629309a8fe1dc17032c7cbea
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-03-28 10:31:21 +0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
     is_vendor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+
     
     
