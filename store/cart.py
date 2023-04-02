495d8665542e72ee2ea634979ac7ec7f0b32245e
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-02 14:58:36 +0500

    Update cart.py

--- store/cart.py
+++ store/cart.py
@@ -9,7 +9,7 @@ class Cart(object):
 
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
-            
+        
         self.cart = cart
 
     def __iter__(self):
@@ -17,11 +17,10 @@ class Cart(object):
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
         
         for item in self.cart.values():
-            item['total_price'] = int(item['product'].price * item['quantity'])
+            item['total_price'] = int(item['product'].price * item['quantity']) / 100
 
             yield item
-
-
+    
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
     
@@ -33,30 +32,30 @@ class Cart(object):
         product_id = str(product_id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 1, 'id': product_id}
+            self.cart[product_id] = {'quantity': int(quantity), 'id': product_id}
         
         if update_quantity:
             self.cart[product_id]['quantity'] += int(quantity)
+            
             if self.cart[product_id]['quantity'] == 0:
                 self.remove(product_id)
-
+        
         self.save()
-
+    
     def remove(self, product_id):
         if product_id in self.cart:
             del self.cart[product_id]
+
             self.save()
-
-
+    
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
 
-
     def get_total_cost(self):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
-
-        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values()))
+        
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) / 100
 
 
