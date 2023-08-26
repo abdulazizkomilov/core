d1aed89815c65d34db6d16ffcf6698b60ca87468
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-26 15:56:33 +0500

    adds

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
