666ff80d92865135f01ac6975670c5019517fc5f
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-26 15:49:42 +0500

    adds

--- core/settings.py
+++ core/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
